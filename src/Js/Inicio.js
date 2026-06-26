38166058203e49c02a64d1ce37831941f4390738
Jarito-arch <jaris.alday10@gmail.com>
2026-06-26 03:57:13 -0500

    Adding reference Calendario button

---
+++ src/Js/Inicio.js
@@ -0,0 +1,9 @@
+const btnTabla = document.getElementById("btnTablaPosiciones");
+btnTabla.addEventListener("click", () => {
+  window.location.href = "partidos.html#tabla-posiciones";
+});
+
+const btnCalendario = document.getElementById("btnCalendario");
+btnCalendario.addEventListener("click", () => {
+  window.location.href = "partidos.html#calendario";
+});
