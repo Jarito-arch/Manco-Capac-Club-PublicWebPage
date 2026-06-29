03edcc74187e92d978dd6c571df30e9b3cc7810b
PercyCodex <Aprendizaje444@gmail.com>
2026-06-29 00:27:41 -0500

    I created a `global.js` file to handle the hamburger menu, and I configured the global CSS and all the HTML files that will include it for mobile screen sizes

---
+++ src/Js/global.js
@@ -0,0 +1,6 @@
+const boton = document.getElementById("hamburger-menu");
+const menu = document.querySelector(".navPrincipal");
+
+boton.addEventListener("click", () => {
+    menu.classList.toggle("active");
+});
