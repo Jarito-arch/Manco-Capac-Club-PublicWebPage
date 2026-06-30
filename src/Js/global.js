d382f69700e8c8952fe8a09802593903efdbb6ff
fiorellaVelez-prog <yoloaventuras.fiorella@gmail.com>
2026-06-30 14:19:21 -0500

    update el-club page v2

--- src/Js/global.js
+++ src/Js/global.js
@@ -8,7 +8,7 @@ boton.addEventListener("click", () => {
 window.addEventListener("scroll", () => {
     let scrollMaximo = 500;
     let umbral = 600;
-    document.querySelectorAll('.imagen-historia').forEach(
+    document.querySelectorAll('.item-historia').forEach(
         img => {
             positionY = img.getBoundingClientRect().y;
             opacity = positionY < scrollMaximo ? 1 : positionY < umbral ? 1 - ((positionY - scrollMaximo) / 100) : 0.2;
