50cf5faa60126745c1e8f804151540d8a1c9008e
PercyCodex <Aprendizaje444@gmail.com>
2026-07-08 10:17:06 -0500

    agregue funcionalidad a los filtros de equipos para que se muestren solo los que están hay nada más

--- src/Js/equipos.js
+++ src/Js/equipos.js
@@ -1,12 +1,14 @@
 function abrirModal(img, nombre, info, desc) {
-    if(window.innerWidth >= 769){
+    if(window.innerWidth >= 780){
         document.getElementById("jugadorDesktopImagen").src = img;
         document.getElementById("jugadorDesktopNombre").textContent = nombre;
         let datos = info.split("·");
         document.getElementById("jugadorDesktopNumero").textContent = datos[0] || "#00";
         document.getElementById("jugadorDesktopPosicion").textContent = datos[1] || "POSICIÓN";
         document.getElementById("modalJugadorDesktop").style.display="flex";
+
     }else{
+
         document.getElementById('modal-img').src = img;
         document.getElementById('modal-nombre').textContent = nombre;
         document.getElementById('modal-info').textContent = info;
@@ -31,6 +33,28 @@ document.getElementById('modal').addEventListener('click',function(e){
     }
 });
 
-document
-.querySelector(".cerrar-modal-desktop")
-.addEventListener("click",cerrarModalDesktop);
+document.getElementById("modalJugadorDesktop").addEventListener("click", function(e){
+    if(e.target === this){
+        cerrarModalDesktop();
+    }
+});
+
+document.querySelector(".cerrar-modal-desktop").addEventListener("click",cerrarModalDesktop);
+const botonesCategoria = document.querySelectorAll(".category-button");
+const tarjetas = document.querySelectorAll(".container1");
+
+
+
+botonesCategoria.forEach(boton => {
+    boton.addEventListener("click", function(e){
+        e.preventDefault();
+        const categoria = this.dataset.filtro;
+        tarjetas.forEach(tarjeta=>{
+            if(tarjeta.dataset.categoria === categoria){
+                tarjeta.style.display="";
+            }else{
+                tarjeta.style.display="none";
+            }
+        });
+    });
+});
