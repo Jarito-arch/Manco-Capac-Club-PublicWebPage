0d462739fda0b1f92b172c9e97328c528f3d9e45
PercyCodex <Aprendizaje444@gmail.com>
2026-07-08 01:30:10 -0500

    correcting font, color styles, link to another page

--- src/Js/contact.js
+++ src/Js/contact.js
@@ -9,3 +9,30 @@ radios.forEach(radio => {
     });
 
 });
+
+
+const formulario = document.getElementById("ContentForm");
+
+const modal = document.getElementById("modal");
+
+const loader = document.getElementById("loader");
+
+const success = document.getElementById("success");
+
+
+formulario.addEventListener("submit", function(e){
+    e.preventDefault();
+    modal.classList.add("show");
+    loader.style.display="block";
+    success.style.display="none";
+
+    setTimeout(()=>{
+        loader.style.display="none";
+        success.style.display="block";
+    },1000);
+
+    setTimeout(()=>{
+        modal.classList.remove("show");
+        formulario.reset();
+    },2500);
+});
