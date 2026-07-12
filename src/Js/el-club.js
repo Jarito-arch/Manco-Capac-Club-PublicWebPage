79da1063a557c0cb14a339155d75861a2883ab10
fiorellaVelez-prog <yoloaventuras.fiorella@gmail.com>
2026-07-12 17:44:40 -0500

    Creating a History class and functions to create items for each year of the history

--- src/Js/el-club.js
+++ src/Js/el-club.js
@@ -1,3 +1,90 @@
+class Historia{
+    constructor(datos){
+        this.datos = datos;
+    }
+
+    getHistoria(){
+        return this.datos;
+    }
+};
+
+const lineaTiempo = document.getElementById("lineaTiempo");
+
+const historia = new Historia(dataHistoria);
+
+function crearImagen(ruta){
+
+    let div = document.createElement("div");
+    div.classList.add("imagen-historia");
+
+    let img = document.createElement("img");
+
+    img.src = `../assets/Images/History/${ruta}`;
+
+    div.appendChild(img);
+
+    return div;
+
+};
+
+function crearContenido(item){
+
+    let div = document.createElement("div");
+
+    div.classList.add("contenido");
+
+    let estrella = document.createElement("span");
+    estrella.classList.add("estrella");
+    estrella.textContent = "★";
+
+    let h3 = document.createElement("h3");
+    h3.textContent = item.año;
+
+    let h4 = document.createElement("h4");
+    h4.textContent = item.titulo;
+
+    let p = document.createElement("p");
+    p.textContent = item.descripcion;
+
+    div.append(
+        estrella,
+        h3,
+        h4,
+        p
+    );
+
+    return div;
+
+};
+
+function crearItem(item){
+
+    let div = document.createElement("div");
+
+    div.classList.add("item-historia");
+
+    div.appendChild(crearImagen(item.imagen));
+
+    div.appendChild(crearContenido(item));
+
+    return div;
+
+}
+
+function mostrarHistoria(){
+
+    historia.getHistoria().forEach(item =>{
+
+        lineaTiempo.appendChild(
+            crearItem(item)
+        );
+
+    });
+
+};
+
+mostrarHistoria();
+
 window.addEventListener("scroll", () => {
     let scrollMaximo = 500;
     let umbral = 600;
