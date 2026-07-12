b4dcbf1fd84ec1a331a8c7018814dd3abd0dba82
fiorellaVelez-prog <yoloaventuras.fiorella@gmail.com>
2026-07-12 17:52:36 -0500

    fixing connection to js

--- src/Js/el-club.js
+++ src/Js/el-club.js
@@ -1,3 +1,5 @@
+import { dataHistoria } from "./datosHistoria.js";
+
 class Historia{
     constructor(datos){
         this.datos = datos;
@@ -90,8 +92,8 @@ window.addEventListener("scroll", () => {
     let umbral = 600;
     document.querySelectorAll('.item-historia').forEach(
         img => {
-            positionY = img.getBoundingClientRect().y;
-            opacity = positionY < scrollMaximo ? 1 : positionY < umbral ? 1 - ((positionY - scrollMaximo) / 100) : 0.2;
+            let positionY = img.getBoundingClientRect().y;
+            let opacity = positionY < scrollMaximo ? 1 : positionY < umbral ? 1 - ((positionY - scrollMaximo) / 100) : 0.2;
             opacity = Math.max(0.2, Math.min(1, opacity));
             img.style.opacity = opacity;
         }
