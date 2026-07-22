24ac251383ce33801e4bbf81ea3b81c3e11082d9
PercyCodex <Aprendizaje444@gmail.com>
2026-07-22 18:06:13 -0500

    Add new code analytics.js

--- src/Js/analytics.js
+++ src/Js/analytics.js
@@ -2,11 +2,11 @@
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'G-ZDBQPTTHM9');
+gtag('config', 'G-B0MS7MEBG6');
 
 (function() {
     const script = document.createElement('script');
     script.async = true;
-    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-ZDBQPTTHM9';
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-B0MS7MEBG6';
     document.head.appendChild(script);
 })();
