8a4169ecd3d5fff9f08b4622515da8b02bf81140
PercyCodex <Aprendizaje444@gmail.com>
2026-07-22 17:59:04 -0500

    Add new analytics.js

---
+++ src/Js/analytics.js
@@ -0,0 +1,12 @@
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-ZDBQPTTHM9');
+
+(function() {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-ZDBQPTTHM9';
+    document.head.appendChild(script);
+})();
